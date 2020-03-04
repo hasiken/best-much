f6821870a81cd1c403e3b6063495300a24221f33
hasiken <kentarou.123.99@gmail.com>
2020-03-04 21:03:12 +0900

    agd

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
   root to: 'posts#index' 
-  resources :posts, only: [:new, :create]
+  resources :posts, only: [:new, :create, :show]
+
 end
