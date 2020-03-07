4a717d988437e547129053fe6891d9cb5e940dee
hasiken <kentarou.123.99@gmail.com>
2020-03-07 16:02:51 +0900

    c

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show]
   resources :posts, only: [:new, :create, :show] do
+    collection do
+      get 'short'
+    end
     resources :likes, only: [:create, :destroy]
   end 
   resources :images, only: [:index]
